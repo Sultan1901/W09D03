6a71ba2084f8a71a5214b5ece6d573e294693304
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-07 16:55:24 +0300

    craete copmonents

---
+++ redux2/src/component/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            Register
+        </div>
+    )
+}
+
+export default index
---
+++ redux2/src/component/Task/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            Tasks
+        </div>
+    )
+}
+
+export default index
